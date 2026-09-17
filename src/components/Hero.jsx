29b6c3ecdb2a2c52bab749f45e065f75fd5d334b
Nazmul Hossain <nazmulhossain2905@gmail.com>
2026-09-17 12:37:24 +0600

    feat(hero): Hero component design done

---
+++ src/components/Hero.jsx
@@ -0,0 +1,40 @@
+import { FaArrowRight } from "react-icons/fa6";
+import { IoSearch } from "react-icons/io5";
+import HeroBGImage from "../assets/hero-bg.png";
+
+const Hero = () => {
+  return (
+    <header
+      style={{
+        backgroundImage: `linear-gradient(45deg, rgba(0,0,0,0.7) 30%, transparent) ,url(${HeroBGImage})`,
+      }}
+      className="flex min-h-[90vh] items-center justify-center bg-cover bg-center bg-no-repeat"
+    >
+      <div className="container mx-auto flex flex-col items-start gap-8">
+        <div className="space-y-5">
+          <span className="inline-block text-xs font-bold tracking-widest text-[#ac83e4] uppercase">
+            Discover • Explore • Enjoy
+          </span>
+          <h1 className="text-7xl font-black text-white">
+            Your Next Favorite <br />
+            <span className="text-[#7866f2]">Movie</span> Awaits
+          </h1>
+        </div>
+
+        <p className="max-w-[500px] text-lg font-medium text-gray-400">
+          Explore a world of amazing movies. Discover new releases, read
+          detailed information and find your next favorite film
+          <br />
+          -- all in one place.
+        </p>
+
+        <button className="group mt-3 flex cursor-pointer items-center gap-3 rounded-full bg-linear-to-r from-yellow-500 to-yellow-300 px-5 py-3 font-bold">
+          <IoSearch className="text-2xl" /> Explore Movies{" "}
+          <FaArrowRight className="transition-transform group-hover:translate-x-1" />
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Hero;
